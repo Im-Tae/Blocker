4ff52606b2115acdd2427fb0b63c986c3011a768
Im-Tae <itggood2420@gmail.com>
2022-11-07 18:35:51 +0900

    add: throttle debounce for compose material button

---
+++ blocker/src/main/java/com/leaf/blocker/extend/BlockerButton.kt
@@ -0,0 +1,83 @@
+/*
+ * Create by Im-Tae on 2022. 11. 7.
+ *
+ * Copyright (c) 2022. Im-Tae. All rights reserved.
+ */
+
+package com.leaf.blocker.extend
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Shape
+import com.leaf.blocker.Blocker
+import kotlinx.coroutines.MainScope
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun ThrottleButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    skipInterval: Long = Blocker.getInterval(),
+    enabled: Boolean = true,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+    elevation: ButtonElevation? = ButtonDefaults.elevation(),
+    shape: Shape = MaterialTheme.shapes.small,
+    border: BorderStroke? = null,
+    colors: ButtonColors = ButtonDefaults.buttonColors(),
+    contentPadding: PaddingValues = ButtonDefaults.ContentPadding,
+    content: @Composable RowScope.() -> Unit,
+) {
+    Button(
+        onClick = throttleFirst(
+            skipInterval = skipInterval,
+            coroutineScope = MainScope()
+        ) { onClick() },
+        modifier = modifier,
+        enabled = enabled,
+        interactionSource = interactionSource,
+        elevation = elevation,
+        shape = shape,
+        border = border,
+        colors = colors,
+        contentPadding = contentPadding,
+        content = content
+    )
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun DebounceButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    waitInterval: Long = Blocker.getInterval(),
+    enabled: Boolean = true,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+    elevation: ButtonElevation? = ButtonDefaults.elevation(),
+    shape: Shape = MaterialTheme.shapes.small,
+    border: BorderStroke? = null,
+    colors: ButtonColors = ButtonDefaults.buttonColors(),
+    contentPadding: PaddingValues = ButtonDefaults.ContentPadding,
+    content: @Composable RowScope.() -> Unit,
+) {
+    Button(
+        onClick = debounce(
+            waitInterval = waitInterval,
+            coroutineScope = MainScope()
+        ) { onClick() },
+        modifier = modifier,
+        enabled = enabled,
+        interactionSource = interactionSource,
+        elevation = elevation,
+        shape = shape,
+        border = border,
+        colors = colors,
+        contentPadding = contentPadding,
+        content = content
+    )
+}
